76677c7ea1f492e8be8c777fde555638fb994970
Andrew Hinshaw <andrewthinshaw@gmail.com>
2020-11-28 15:23:22 -0800

    add Inventory model

--- icetrack/models.py
+++ icetrack/models.py
@@ -5,4 +5,26 @@ class Order(models.Model):
     name = models.TextField()
 
     def __str__(self):
-        return self.name[:50]
+        return self.name
+
+class Product(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Inventory(models.Model):
+	item_name = models.CharField(max_length=50, blank=True, null=True)
+	quantity = models.IntegerField(default='0', blank=True, null=True)
+	received_quantity = models.IntegerField(default='0', blank=True, null=True)
+	received_by = models.CharField(max_length=50, blank=True, null=True)
+	sold_quantity = models.IntegerField(default='0', blank=True, null=True)
+	sold_by = models.CharField(max_length=50, blank=True, null=True)
+	sold_to = models.CharField(max_length=50, blank=True, null=True)
+	created_by = models.CharField(max_length=50, blank=True, null=True)
+	reorder_level = models.IntegerField(default='0', blank=True, null=True)
+	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+	export_to_CSV = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.item_name
