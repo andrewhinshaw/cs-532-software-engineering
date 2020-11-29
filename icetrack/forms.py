5c07e3f780f61dda4861f1c796e10cfb5e3148b2
Andrew Hinshaw <andrewthinshaw@gmail.com>
2020-11-29 13:27:22 -0800

    add validation for inventory create

--- icetrack/forms.py
+++ icetrack/forms.py
@@ -6,6 +6,15 @@ class InventoryCreateForm(forms.ModelForm):
         model = Inventory
         fields = ['item_name', 'quantity']
 
+	def clean_item_name(self):
+		item_name = self.cleaned_data.get('item_name')
+		if not item_name:
+            raise forms.ValidationError('This field is required')
+        for instance in Inventory.objects.all():
+            if instance.item_name == item_name:
+                raise forms.ValidationError(str(item_name) + ' is already created')
+    	return item_name
+
 class TicketCreateForm(forms.ModelForm):
     class Meta:
         model = Ticket
